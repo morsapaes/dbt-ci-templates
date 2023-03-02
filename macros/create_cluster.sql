d2d33856d6e3b31280ca15a0adb2554f8a4006d7
morsapaes <marta.paes.moreira@gmail.com>
2023-03-02 11:52:04 +0000

    Test change

---
+++ macros/create_cluster.sql
@@ -0,0 +1,39 @@
+{#
+-- This macro creates a new cluster that will be torn down once the PR is closed (or merged).
+#}
+{% macro create_cluster(cluster_name=env_var('CI_TAG')) %}
+
+  {% if cluster_name %}
+
+    {%- call statement('catalog', fetch_result=True) -%}
+        SELECT name
+        FROM mz_clusters
+        WHERE name = lower('{{ cluster_name }}')
+    {%- endcall -%}
+
+    {%- set result = load_result('catalog') -%}
+
+    {% if result %}
+
+      {{ log("Cluster " ~ cluster_name ~ " already exists, skipping cluster creation...", info=True) }}
+
+    {% else %}
+
+      {{ log("Creating cluster " ~ cluster_name ~ "...", info=True) }}
+
+      {% call statement('create_cluster', fetch_result=True, auto_begin=False) -%}
+          CREATE CLUSTER {{ cluster_name }} REPLICAS (r1 (SIZE = '2xsmall'))
+      {%- endcall %}
+
+      {%- set result = load_result('create_cluster') -%}
+      {{ log(result['data'][0][0], info=True)}}
+
+    {% endif %}
+
+  {% else %}
+
+    {{ exceptions.raise_compiler_error("Invalid arguments. Missing cluster name!") }}
+
+  {% endif %}
+
+{% endmacro %}
--- models/sources/items.sql
+++ models/sources/items.sql
@@ -1,9 +1,7 @@
 {{ config(materialized='source', indexes=[{'columns': ['id']}]) }}
 
 CREATE SOURCE {{ this }}
-FROM KAFKA CONNECTION {{ target.schema }}.kafka_connection (
-    TOPIC 'mysql.shop.items'
-)
+FROM KAFKA CONNECTION {{ target.schema }}.kafka_connection ( TOPIC 'mysql.shop.items' )
 FORMAT AVRO
 USING CONFLUENT SCHEMA REGISTRY CONNECTION {{ target.schema }}.csr_connection
 ENVELOPE DEBEZIUM
