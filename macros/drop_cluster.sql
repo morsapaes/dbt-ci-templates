42bc4cb23c6d10e5789af345d61dba70cc3695c5
morsapaes <marta.paes.moreira@gmail.com>
2023-03-23 04:02:31 +0100

    Tweak macros

--- macros/drop_cluster.sql
+++ macros/drop_cluster.sql
@@ -3,16 +3,16 @@
 #}
 {% macro drop_cluster(cluster_name=env_var('CI_TAG')) %}
 
-  {% if cluster_name %}
+{%- if cluster_name -%}
 
-      {{ log("Dropping cluster " ~ cluster_name ~ "...", info=True) }}
+  {{ log("Dropping cluster " ~ cluster_name ~ "...", info=True) }}
 
-      {% call statement('create_cluster', fetch_result=True, auto_begin=False) -%}
-          DROP CLUSTER {{ cluster_name }} IF EXISTS CASCADE
-      {%- endcall %}
+  {% call statement('create_cluster', fetch_result=True, auto_begin=False) -%}
+      DROP CLUSTER IF EXISTS {{ cluster_name }} CASCADE
+  {%- endcall %}
 
-      {%- set result = load_result('create_cluster') -%}
-      {{ log(result['data'][0][0], info=True)}}
+  {%- set result = load_result('create_cluster') -%}
+  {{ log(result['data'][0][0], info=True)}}
 
   {% else %}
 
