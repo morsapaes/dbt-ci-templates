e2793001f3994d8c82cdc78674375e0d5b50209a
morsapaes <marta.paes.moreira@gmail.com>
2023-09-27 11:13:48 -0700

    Test Slim CI

--- models/staging/winning_bids.sql
+++ models/staging/winning_bids.sql
@@ -4,7 +4,7 @@ SELECT DISTINCT ON
   (auctions.id) bids.*,
   auctions.item,
   auctions.seller
-FROM {{ source('auction','auctions') }}
-JOIN {{ source('auction','bids') }} ON auctions.id = bids.auction_id
+FROM {{ source('auction','auctions') }} auctions
+JOIN {{ source('auction','bids') }} bids ON auctions.id = bids.auction_id
 WHERE bids.bid_time < auctions.end_time
   AND mz_now() >= auctions.end_time
