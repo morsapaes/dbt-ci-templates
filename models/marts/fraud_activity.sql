ce9b12e4977563a75f2024a30f0ae9058d2d84d0
morsapaes <marta.paes.moreira@gmail.com>
2024-01-16 12:03:23 +0100

    Test

--- models/marts/fraud_activity.sql
+++ models/marts/fraud_activity.sql
@@ -6,6 +6,6 @@ SELECT
   w2.amount AS seller_amount,
   w1.item buyer_item,
   w1.amount buyer_amount
-FROM {{ ref('winning_bids') }} w1
+FROM {{ ref('dummy') }} w1
 JOIN {{ ref('winning_bids') }} w2 ON w1.buyer = w2.seller
 WHERE w2.amount > w1.amount
